bebeb9aaf21dddbfbd07ec99ecb7036a1401ef8a
hyewwon <rlagp424@gmail.com>
2023-11-17 17:57:51 +0900

    chore : add permisstion model

--- website/models.py
+++ website/models.py
@@ -1,9 +1,63 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.query_utils import Q
 from datetime import datetime
 
+class AccessPermissionManager(models.Manager):
+    def get_queryset(self):
+        return super(AccessPermissionManager, self).\
+            get_queryset().filter(
+                Q(content_type=AccessPermission.PERMISSION_TYPE.READ) |
+                Q(content_type=AccessPermission.PERMISSION_TYPE.WRITE)
+            )
+
+
+class AccessPermission(Permission):
+    """Proxy model for permission"""
+
+    class Meta:
+        proxy = True
+
+    class __PERMISSION_TYPE:
+        def get_all_permissions(self):
+                    """Get list of all permissions."""
+                    return [
+                        self.READ,
+                        self.WRITE
+                    ]
+
+        @property
+        def WRITE(self):
+            return ContentType.objects.get_or_create(
+                app_label='write',
+                model="write_permission",
+            )[0]
+
+        @property
+        def READ(self):
+            return ContentType.objects.get_or_create(
+            app_label='read',
+            model='read_permission',
+            )[0]
+
+    PERMISSION_TYPE = __PERMISSION_TYPE()
+    objects = AccessPermissionManager()
+    
+    def save(self, *args, **kwargs):
+        # Check Permission Type.
+        if self.content_type not in self.PERMISSION_TYPE.get_all_permissions():
+            raise ValueError('Not permitted Permission Type.')
+        
+        # ct, created = ContentType.objects.get_or_create(
+        #     model=self._meta.verbose_name, app_label=self._meta.app_label,
+        # )
+
+        #self.content_type = ct
+        super(AccessPermission, self).save(*args)
+
+# 소속 회사
 class Company(models.Model):
     name = models.CharField(db_column="name", null=False, max_length=100)
     image = models.ImageField(db_column="image", upload_to="company_images", null=True)
@@ -21,25 +75,16 @@ class Department(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    company = models.ForeignKey(Company, on_delete=models.SET_NULL, related_name="profile", null=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="profile", null=True)
     reg_root = models.CharField(db_column="reg_root", null=False, max_length=15, default="")
     phone_no = models.CharField(db_column="phone_no", null=True, max_length=15, default="")
-    image = models.ImageField(db_column="image", upload_to="profile_images")
+    image = models.ImageField(db_column="image", upload_to="profile_images", null=True)
     note = models.TextField(db_column="note",null=True)
     check_flag = models.CharField(db_column="check_flag", max_length=10, default="0")
 
     class Meta:
-        db_table = "profile"
+        db_table = "auth_profile"
         
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
 
 # 휴가 
 class Vacation(models.Model):
@@ -97,6 +142,7 @@ class BoradReply(models.Model):
         db_table = "board_reply"
 
 
+# 공지사항
 class Notice(models.Model):
     title = models.CharField(db_column="title", null=False, max_length=255)
     content = models.TextField(db_column="content", null=False)
